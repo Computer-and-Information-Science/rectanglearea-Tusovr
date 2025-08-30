19e5320c24940bf8ad83116ee3b147c45d3913ee
Maurice Brown <149290291+Tusovr@users.noreply.github.com>
2025-08-30 22:23:55 +0000

    Added the remaining properties to display the area and perimeter.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
+  const double perimeter = 2 * (length + width);
+  const double area = length * width;
+  
+  cout << "Rectangle Properties:" << endl;
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
+
+  return 0;
 
 
 }
